9782fe14014e69a6e563d6e4e90116a8f43fb927
qpslqlcl12 <kanghyun101@gmail.com>
2021-12-10 23:58:08 +0900

    commit message

--- mnist.py
+++ mnist.py
@@ -1,1 +1,8 @@
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+print(train_images.shape)
