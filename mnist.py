2c78d15eab044e0a2d2fe840488324aec4862d8b
qpslqlcl12 <kanghyun101@gmail.com>
2021-12-11 16:13:31 +0900

    commit

--- mnist.py
+++ mnist.py
@@ -5,4 +5,8 @@ import matplotlib.pyplot as plt
 mnist = tf.keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-print(train_images.shape)
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.savefig('test.png', dpi=300)
