5169279e985bda8e22df16e94560a65c9087d448
qpslqlcl12 <kanghyun101@gmail.com>
2021-12-12 23:18:33 +0900

    commit

--- mnist.py
+++ mnist.py
@@ -1,12 +1,39 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
 
-mnist = tf.keras.datasets.mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+inputs = keras.Input(shape=(784,))
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.savefig('test.png', dpi=300)
+dense = layers.Dense(64, activation="relu")
+x = dense(inputs)
+x = layers.Dense(64, activation="relu")(x)
+outputs = layers.Dense(10)(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs, name="mnist_model")
+#model.summary()
+
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x_train.reshape(60000, 784).astype("float32") / 255
+x_test = x_test.reshape(10000, 784).astype("float32") / 255
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.SGD(learning_rate=0.01),
+    metrics=["accuracy"],
+)
+
+history = model.fit(x_train, y_train, batch_size=64, epochs=2, validation_split=0.2)
+
+test_scores = model.evaluate(x_test, y_test, verbose=2)
+print("Test loss:", test_scores[0])
+print("Test accuracy:", test_scores[1])
+
+#model.fit(train_images, train_labels, epochs=1)  
+#model.evaluate(train_images, train_labels)
+#model.train_on_batch(train_images, train_labels )
+#model.test_on_batch(train_images, train_labels)
+#model.evaluate(train_images, train_labels)
