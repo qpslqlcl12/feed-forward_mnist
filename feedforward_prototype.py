0c626163a6f07a4db74e84883c10e21f0a2378cd
qpslqlcl12 <kanghyun101@gmail.com>
2021-12-16 00:58:49 +0900

    commit

--- feedforward_prototype.py
+++ feedforward_prototype.py
@@ -11,13 +11,14 @@ numtrainimages = 1000
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 x_train = x_train.reshape(60000, 784).astype("float32") / 255
 x_test = x_test.reshape(10000, 784).astype("float32") / 255
-       
 
-inputs = keras.Input(shape=(784,), name="digits")
-x1 = layers.Dense(64, activation="relu")
-outputs = layers.Dense(10, name="predictions")(x1)
 
-print(inputs)   #tensorshape (none, 784)
+W1 = tf.Variable(tf.ones(shape=(784,400)))
+
+@tf.function
+def model(x) :
+    return W1 * Input
+print(W1) 
 
 """
 for epochs in range(1, numepoch):
