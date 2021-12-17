36901bbd5310c0d8fb9c6e8c802290b5fce49701
qpslqlcl12 <kanghyun101@gmail.com>
2021-12-17 16:22:26 +0900

    commit

--- feedforward_prototype.py
+++ feedforward_prototype.py
@@ -9,17 +9,18 @@ import time
 x_train = x_train.reshape(60000, 784).astype("float32") / 255
 x_test = x_test.reshape(10000, 784).astype("float32") / 255
 batch_size=32
-
+print("train_label = ", y_train[1])
 
 # Prepare the training dataset.
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))      #[x_train, y_train]
 train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)   #put in buffer 1024 data
 
+
+
 inputs = keras.Input(shape=(784,))
-dense = layers.Dense(784, activation="sigmoid")
-x = dense(inputs)
-x = layers.Dense(400, activation="sigmoid")(x)
-outputs = layers.Dense(10)(x)
+x1 = layers.Dense(784, activation="sigmoid")(inputs)
+x2 = layers.Dense(400, activation="sigmoid")(x1)
+outputs = layers.Dense(10)(x2)
 
 model = keras.Model(inputs=inputs, outputs=outputs, name="mnist_model")
 
@@ -28,7 +29,7 @@ loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 def loss(model, x, y, training):
     # training=training is needed only if there are layers with different
     # behavior during training versus inference (e.g. Dropout).
-    y_ = model(x, training=training)
+    y_ = model(x, training=training)        # logit(y)= log(y/(1-y))
 
     return loss_object(y_true=y, y_pred=y_)
 
@@ -38,17 +39,13 @@ print("Loss test: {}".format(l))
 def grad(model, inputs, targets):
     with tf.GradientTape() as tape:
         loss_value = loss(model, inputs, targets, training=True)
-    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+    return loss_value, tape.gradient(loss_value, model.trainable_variables) #dloss_value/dmodel.trainable_variables
 
 optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
-
 loss_value, grads = grad(model, x_train, y_train)
-
 print("Step: {}, Initial Loss: {}".format(optimizer.iterations.numpy(),
                                           loss_value.numpy()))
-
 optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
 print("Step: {},         Loss: {}".format(optimizer.iterations.numpy(),
                                           loss(model, x_train, y_train, training=True).numpy()))
 
@@ -59,7 +56,7 @@ print("Step: {},         Loss: {}".format(optimizer.iterations.numpy(),
 train_loss_results = []
 train_accuracy_results = []
 
-num_epochs = 2
+num_epochs = 1
 
 for epoch in range(num_epochs):
     epoch_loss_avg = tf.keras.metrics.Mean()
@@ -78,6 +75,9 @@ for epoch in range(num_epochs):
     # training=True is needed only if there are layers with different
     # behavior during training versus inference (e.g. Dropout).
         epoch_accuracy.update_state(y, model(x, training=True))
+
+        if epoch % 1 == 0:
+            print("label =", y)
 
   # End epoch
     train_loss_results.append(epoch_loss_avg.result())
