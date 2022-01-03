05f310ebb4f9ac37fdf71b6311e948d9b3401bb4
qpslqlcl12 <kanghyun101@gmail.com>
2022-01-03 22:46:35 +0900

    home

--- recoginition.py
+++ recoginition.py
@@ -5,9 +5,15 @@ import numpy as np
 import time
 
 #data=([])
-input = tf.Variable(tf.zeros(shape=(50), dtype=tf.float32))
+label = tf.Variable([0.,0.,0.,0.,0.],[0.,0.,1.,0.,0.])
 
-w1=tf.Variable(tf.random.normal([50], dtype=tf.float32), name='w1')
-
-h1=tf.matmul(input, w1)
+input = tf.Variable(tf.zeros(shape=(1,5), dtype=tf.float32))
+print(input)
+conc=tf.concat([label,input], -1)
+print(conc)
+w1=tf.Variable(tf.random.normal([5,2],stddev=0.35, dtype=tf.float32), name='w1')
+print(w1)
+h1=tf.matmul(input,w1  )
+print(h1)
+h1=tf.keras.activations.sigmoid(h1)
 print(h1)
